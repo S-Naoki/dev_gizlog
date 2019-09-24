9beb8cfd2fa3c27eeddcf48b7e37b28c6be96d7b
S-Naoki <s.nao0919@gmail.com>
2019-09-24 17:56:13 +0900

    カンマ後に半角スペースを入れ忘れていたので、追加しました。

--- resources/views/user/daily_report/index.blade.php
+++ resources/views/user/daily_report/index.blade.php
@@ -26,7 +26,7 @@
             <td class="col-xs-2">{{ $dailyReport->reporting_time->format('m/d (D)') }}</td>
             <td class="col-xs-3">{{ $dailyReport->title }}</td>
             <td class="col-xs-5">{{ $dailyReport->content }}</td>
-            <td class="col-xs-2"><a class="btn" href="{{ route('report.show',$dailyReport->id) }}"><i class="fa fa-book"></i></a></td>
+            <td class="col-xs-2"><a class="btn" href="{{ route('report.show', $dailyReport->id) }}"><i class="fa fa-book"></i></a></td>
           </tr>
         @endforeach
       </tbody>
