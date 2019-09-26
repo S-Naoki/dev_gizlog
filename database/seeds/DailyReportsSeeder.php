9639c0ea88d846bcf390c34e9fb3f6515aeda839
S-Naoki <s.nao0919@gmail.com>
2019-09-26 12:10:29 +0900

    truncate処理を追加しました。初期投入するレコードを追加しました

--- database/seeds/DailyReportsSeeder.php
+++ database/seeds/DailyReportsSeeder.php
@@ -11,14 +11,26 @@ class DailyReportsSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('daily_reports')->truncate();
         DB::table('daily_reports')->insert([
-            'id' => 2,
-            'user_id' => 4,
-            'title' => 'テストタイトル',
-            'content' => 'テスト内容',
-            'reporting_time' => Carbon::create(2019,1,2),
-            'created_at' => Carbon::create(2019, 1, 3),
-            'updated_at' => Carbon::create(2018, 1, 4)
-        ]);
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'title' => '朝ごはん',
+                'content' => '今日は朝ごはんを食べました。',
+                'reporting_time' => Carbon::create(2019,1,2),
+                'created_at' => Carbon::create(2019, 1, 1),
+                'updated_at' => Carbon::create(2018, 1, 2)
+    
+            ],
+            [
+                'id' => 2,
+                'user_id' => 4,
+                'title' => 'テストタイトル',
+                'content' => 'テスト内容',
+                'reporting_time' => Carbon::create(2019,1,2),
+                'created_at' => Carbon::create(2019, 1, 3),
+                'updated_at' => Carbon::create(2018, 1, 4)
+            ]]);
     }
 }
