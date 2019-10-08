ced033516690588cfcd4a1843f05b72ceda2f880
S-Naoki <s.nao0919@gmail.com>
2019-10-08 16:18:30 +0900

    Commentモデルを作成しました。

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'tag_category_id',
+        'question_id',
+        'comment'
+    ];
+
+    
+    public function user ()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
