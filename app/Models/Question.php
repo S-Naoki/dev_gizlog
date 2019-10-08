57da654193ed91fd7dd4219e7e8e6bcfa673ccff
S-Naoki <s.nao0919@gmail.com>
2019-10-08 16:17:27 +0900

    Questionモデルを作成しました。

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,9 +3,76 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\TagCategory;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
-    //
+    use softDeletes;
+    
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'tag_category_id',
+    ];
+    
+    protected $dates = [
+        'deleted_at'
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function tagCategory()
+    {
+        return $this->belongsTo(TagCategory::class);
+    }
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function fetchQuestionsByUserId($id)
+    {
+        return $this->where('user_id', $id)
+                    ->orderby('created_at', 'asc')
+                    ->get();
+                    
+        
+    }
+    
+    public function searchQuestion($inputs, $tagCategoryId)
+    {
+        if (!empty($inputs)) {
+            $questions = $this->searchQuestionByKeyWord($inputs['search_word'])
+                            ->searchQuestionByTagCategory($inputs, $tagCategoryId)
+                            ->get();
+        } else {
+            $questions = $this->all()->sortBy('created_at');
+        }
+        return $questions;
+    }
+    
+    public function scopeSearchQuestionByTagCategory($query, $inputs, $tagCategoryId)
+    {
+        if (!empty($inputs['tag_category_id'])) {
+            return $query->where('tag_category_id', $tagCategoryId)
+            ->orderby('created_at', 'asc');
+        }
+
+    }
+    
+    public function scopeSearchQuestionByKeyWord($query, $searchWord)
+    {
+        return $query->where('title', 'like', '%' .$searchWord. '%')
+        ->orderby('created_at', 'asc');
+    }
+
 }
 
