666b3e6d6b18804517ba83732c901ac939ae971a
S-Naoki <s.nao0919@gmail.com>
2019-10-10 18:22:13 +0900

    新しいフォームリクエストクラスを作成しました。

---
+++ app/Http/Requests/User/SearchQuestionsRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchQuestionsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'tag_category_id' => 'sometimes|integer',
+            'search_word'     => 'max:50'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'search_word.max' => '50字以内で入力してください。'
+        ];
+    }
+}
