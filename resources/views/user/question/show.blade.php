35e17dc73a2ef3b430eabf4a5264d57c6470c3d2
S-Naoki <s.nao0919@gmail.com>
2019-10-11 10:44:38 +0900

    エラーメッセージを表示させるための記述を追記しました。

--- resources/views/user/question/show.blade.php
+++ resources/views/user/question/show.blade.php
@@ -42,9 +42,9 @@
       <div class="comment-title">
         <img src="{{ $user->avatar }}" class="avatar-img"><p>コメントを投稿する</p>
       </div>
-      <div class="comment-body">
+      <div class="comment-body @if(!empty($errors->has('comment'))) has-error @endif">
         {!! Form::textarea('comment', null, ['class' => 'form-control', 'placeholder' => 'Add your comment...']) !!}
-        <span class="help-block"></span>
+        <span class="help-block">{{ $errors->first('comment') }}</span>
       </div>
       <div class="comment-bottom">
         {!! Form::button('<i class="fa fa-pencil" aria-hidden="true"></i>', ['class' => 'btn btn-success', 'type' => 'submit']) !!}
