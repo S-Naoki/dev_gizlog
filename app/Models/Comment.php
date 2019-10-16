8dcab78e855b23e2ad85200156cecb14190cf31a
S-Naoki <s.nao0919@gmail.com>
2019-10-16 11:01:23 +0900

    $fillableの内容修正をしました。

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,15 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = [
-        'title',
-        'content',
         'user_id',
-        'tag_category_id',
         'question_id',
         'comment'
     ];
     
-    public function user ()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
