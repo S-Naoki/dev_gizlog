00ee334706b26e6f232904dcce16cbff72b5a047
S-Naoki <s.nao0919@gmail.com>
2019-10-17 09:53:07 +0900

    アローの位置を修正しました。

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -14,25 +14,25 @@ class CommentsTableSeeder extends Seeder
         DB::table('comments')->truncate();
         DB::table('comments')->insert([
             [
-                'user_id'     => 1,
-                'question_id' => 1,
-                'comment'     => 'テストコメント',
+                'user_id'         => 1,
+                'question_id'     => 1,
+                'comment'         => 'テストコメント',
                 'created_at'      => Carbon::create(2019,1,1),
-                'updated_at'       => Carbon::create(2019,1,2)
+                'updated_at'      => Carbon::create(2019,1,2)
             ],
             [
-                'user_id'     => 2,
-                'question_id' => 2,
-                'comment'     => 'テストコメント2',
-                'created_at'      => Carbon::create(2019,2,1),
-                'updated_at'       => Carbon::create(2019,2,2)
+                'user_id'           => 2,
+                'question_id'       => 2,
+                'comment'           => 'テストコメント2',
+                'created_at'        => Carbon::create(2019,2,1),
+                'updated_at'        => Carbon::create(2019,2,2)
             ],
             [
-                'user_id'     => 3,
-                'question_id' => 3,
-                'comment'     => 'テストコメント3',
+                'user_id'         => 3,
+                'question_id'     => 3,
+                'comment'         => 'テストコメント3',
                 'created_at'      => Carbon::create(2019,3,1),
-                'updated_at'       => Carbon::create(2019,3,2)
+                'updated_at'      => Carbon::create(2019,3,2)
             ]
         ]);
     }
