c71245eb4c424f870115085c93a7934b04cbc9fd
S-Naoki <s.nao0919@gmail.com>
2019-10-31 10:12:34 +0900

    マイグレーションファイルを作成しました。

---
+++ database/migrations/2019_10_23_054507_create_attendances_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->date('date');
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
+            $table->tinyInteger('absence_state')->default(0);
+            $table->string('absence_reason', 500)->nullable();
+            $table->tinyInteger('request_state')->default(0);
+            $table->string('request_content', 500)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
